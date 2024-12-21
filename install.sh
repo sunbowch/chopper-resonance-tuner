494c71100d067133bc4d4971498975fb668897b0
sunbowch <sunbow@bluewin.ch>
2024-12-21 22:40:45 +0100

    tuned to my repo

--- install.sh
+++ install.sh
@@ -67,8 +67,8 @@ if [ -f "$blk_path" ]; then
           sed -i "\$a [update_manager $repo]" "$blk_path"
           sed -i "\$a type: git_repo" "$blk_path"
           sed -i "\$a path: $repo_path" "$blk_path"
-          sed -i "\$a origin: https://github.com/MRX8024/$repo.git" "$blk_path"
-          sed -i "\$a primary_branch: main" "$blk_path"
+          sed -i "\$a origin: https://github.com/sunbowch/$repo.git" "$blk_path"
+          sed -i "\$a primary_branch: dev" "$blk_path"
           sed -i "\$a managed_services: klipper" "$blk_path"
           # echo "Including [update_manager] to $blk_path successfully complete"
           sudo service moonraker start
